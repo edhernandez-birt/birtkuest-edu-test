fbe8a3d69cdcc6b712c061589cab5b74f833afe0
edhernandez-birt <edhernandez@birt.eus>
2023-11-06 21:43:39 +0100

    Animacion player lanzamiento hecha
    Animacion player lanzamiento hecha

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveInput;
     private Animator playerAnimator;
 
+    [SerializeField] private GameObject proyectilPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +24,19 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;//Para igualar velocidad diagonal
-        playerAnimator.SetFloat("Horizontal", moveX);
-        playerAnimator.SetFloat("Vertical", moveY);
+        playerAnimator.SetFloat("MoveHorizontal", moveX);
+        playerAnimator.SetFloat("MoveVertical", moveY);
         playerAnimator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+
+        AimAndShoot();
+
+        // Vector3 aim = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0.0f);
+        playerAnimator.SetFloat("AimHorizontal", moveX);
+        playerAnimator.SetFloat("AimVertical", moveY);
+        playerAnimator.SetFloat("AimMagnitude", moveInput.magnitude);
+        playerAnimator.SetBool("Aim", Input.GetButton("Fire1"));
+
 
     }
 
@@ -32,5 +44,26 @@ public class PlayerController : MonoBehaviour
     {
         //Físicas
         playerRb.MovePosition(playerRb.position + moveInput * speed * Time.fixedDeltaTime);
+
+    }
+
+    //https://www.youtube.com/watch?v=aXtd5KFf_iE
+    private void AimAndShoot()
+    {
+     //   Vector3 aim = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        Vector2 shootingDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+     //   if (aim.magnitude > 0.0f)
+     //   {
+           // aim.Normalize();
+         shootingDirection.Normalize();
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                GameObject proyectil = Instantiate(proyectilPrefab,transform.position,Quaternion.identity);
+                proyectil.GetComponent<Rigidbody2D>().velocity = shootingDirection *5.0f;
+                proyectil.transform.Rotate(0,0,Mathf.Atan2(shootingDirection.y,shootingDirection.x)*Mathf.Rad2Deg);
+                Destroy(proyectil, 3.0f);
+            }
+      //  }
     }
 }
