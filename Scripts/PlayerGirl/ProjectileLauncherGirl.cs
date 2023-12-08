a3671b73e482dfcd2a0233a3ce0b9c6289311495
edhernandez-birt <edhernandez@birt.eus>
2023-12-08 14:46:53 +0100

    Disparo adaptado Input moderno
    Disparo de personaje del bosque mejorado y adaptado al nuevo input.

---
+++ Scripts/PlayerGirl/ProjectileLauncherGirl.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncherGirl : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+   // [SerializeField] private GameObject serverProjectilePrefab;
+    [SerializeField] private GameObject projectilePrefab;
+     private Collider2D playerCollider;
+     private Animator playerAnimator;
+
+    [Header("Settings")]
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float fireRate;
+    [SerializeField] private float firingTime = 0.3f;
+
+    private PlayerControllerGirl playerControllerGirl;
+
+    private bool shouldFire = false;
+    private float previousFireTime = 0f;
+
+    private void HandlePrimaryFire(bool shouldFire)
+    {
+        this.shouldFire = shouldFire;
+    }
+
+    private void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
+
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+
+        playerControllerGirl = GetComponent<PlayerControllerGirl>();
+
+        previousFireTime = Time.time;
+    }
+
+    private void Update()
+    {
+
+        if ((Time.time - previousFireTime > firingTime) && playerAnimator.GetBool("isLaunching"))
+        {
+            playerAnimator.SetBool("isLaunching", false);
+
+            Vector2 direction = playerControllerGirl.LastDirection;
+
+            SpawnDummyProjectile(transform.position, direction);
+        }
+
+        if (!shouldFire) { return; }
+
+        if ((Time.time - previousFireTime >= 1 / fireRate) && !playerAnimator.GetBool("isLaunching"))
+        {
+            previousFireTime = Time.time;
+            playerAnimator.SetBool("isLaunching", true);
+        }
+
+    }
+        
+
+   
+
+ 
+    private void SpawnDummyProjectile(Vector3 spawnPos, Vector3 direction)
+    {
+        //Debug.Log("SpawnDummyProjectile 0");
+        // Cuando está pegado a una pared si dispara se choca la piedra, por eso la instancio separada
+        // Si la dirección es hacia arriba, no lo modifico porque dispararía a la pared la posición de pintado importa
+        if(direction != Vector3.up) { spawnPos.y = spawnPos.y - 0.1f; }
+
+        GameObject projectileInstance = Instantiate(
+            projectilePrefab, 
+            spawnPos, 
+            Quaternion.identity);
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = direction * projectileSpeed;
+        }
+
+    }
+
+
+}
