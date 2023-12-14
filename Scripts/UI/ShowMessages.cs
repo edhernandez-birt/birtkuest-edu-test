8634dbbb78ed036f7a6f1837eca93b796ec65e2e
edhernandez-birt <edhernandez@birt.eus>
2023-12-14 16:26:06 +0100

    Pruebas mostrar textos
    Muestra texto OnCollision en lugar de OnTrigger

--- Scripts/UI/ShowMessages.cs
+++ Scripts/UI/ShowMessages.cs
@@ -10,6 +10,45 @@ public class ShowMessages : MonoBehaviour
     public TextMeshProUGUI messageText;
     public Image messageImage;
 
+ //   [SerializeField] float timeBtwnChars;
+ //   [SerializeField] float timeBtwnWords;
+
+    void Start()
+    {
+    }
+
+    //Mostrar letra a letra. No va bien después del primer intento
+    /*
+    public void EndCheck()
+    {
+            StartCoroutine(TextVisible());
+    }
+
+    private IEnumerator TextVisible()
+    {
+        messageText.ForceMeshUpdate();
+        int totalVisibleCharacters = messageText.textInfo.characterCount;
+        int counter = 0;
+
+        while (true)
+        {
+            int visibleCount = counter % (totalVisibleCharacters + 1);
+            messageText.maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleCharacters)
+            {
+                Invoke("EndCheck", timeBtwnWords);
+                break;
+            }
+
+            counter += 1;
+            yield return new WaitForSeconds(timeBtwnChars);
+
+
+        }
+    }*/
+
+    /*
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Se ha producido un trigger con " + other.gameObject + " de " + this.gameObject);
@@ -30,6 +69,28 @@ public class ShowMessages : MonoBehaviour
         Debug.Log("Se ha producido un trigger Exit en " + this.gameObject);
         messageText.enabled = false;
         messageImage.enabled = false;
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Se ha producido una colision con " + this.gameObject);
+
+        {
+            messageText.enabled = true;
+            messageImage.enabled = true;
+           // EndCheck();
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Se ha dejado de producir una colision con " + this.gameObject);
+
+        {
+            messageText.enabled = false;
+            messageImage.enabled = false;
+        }
     }
 
 }
